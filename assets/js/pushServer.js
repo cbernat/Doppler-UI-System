fbf8fb9dd463f31b7b1c980eb678d31bdd2ed96f
cbernat <cbernat@makingsense.com>
2020-03-12 13:24:22 -0300

    -

---
+++ assets/js/pushServer.js
@@ -0,0 +1,47 @@
+const webPush = require('web-push');
+
+/*if (!process.env.VAPID_PUBLIC_KEY || !process.env.VAPID_PRIVATE_KEY) {
+  console.log("You must set the VAPID_PUBLIC_KEY and VAPID_PRIVATE_KEY "+
+    "environment variables. You can use the following ones:");
+  console.log(webPush.generateVAPIDKeys());
+  return;
+}*/
+
+process.env.VAPID_PUBLIC_KEY = "BFtBVunx6Lg_4ziA6b_Oe-9iqQetudiBzkukb1UOOWItZlbdPXCnMLjTfM6gnbmzrusaGlifwWWaFRWnef_H40Y";
+process.env.VAPID_PRIVATE_KEY = "F1eZLlbqpH5d8nHetf6xzzBnlgq8WbFlQ2A9jEl9X4E";
+ 
+webPush.setVapidDetails(
+  'http://localhost:3000/',
+  process.env.VAPID_PUBLIC_KEY,
+  process.env.VAPID_PRIVATE_KEY
+);
+
+module.exports = function(app, route) {
+  app.get(route + 'vapidPublicKey', function(req, res) {
+    res.send(process.env.VAPID_PUBLIC_KEY);
+  });
+
+  app.post(route + 'register', function(req, res) {
+ 
+    res.sendStatus(201);
+  });
+
+  app.post(route + 'sendNotification', function(req, res) {
+    const subscription = req.body.subscription;
+    const payload = req.body.payload;
+    const options = {
+      TTL: 0
+    };
+
+    setTimeout(function() {
+      webPush.sendNotification(subscription, payload, options)
+      .then(function() {
+        res.sendStatus(201);
+      })
+      .catch(function(error) {
+        console.log(error);
+        res.sendStatus(500);
+      });
+    }, req.body.delay * 1000);
+  });
+};
